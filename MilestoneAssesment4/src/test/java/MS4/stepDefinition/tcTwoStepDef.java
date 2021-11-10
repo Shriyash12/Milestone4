e7ff07053e0e3b278eea791b2527bd17044424c5
Shriyash <shri12nil@gmail.com>
2021-11-10 21:51:33 +0530

    2nd commit

--- MilestoneAssesment4/src/test/java/MS4/stepDefinition/tcTwoStepDef.java
+++ MilestoneAssesment4/src/test/java/MS4/stepDefinition/tcTwoStepDef.java
@@ -1,29 +1,79 @@
-/*
- * package MS4.stepDefinition;
- * 
- * import org.openqa.selenium.Keys;
- * 
- * import MS4.POM.testCaseTwoPOM; import MS4.base.BaseClass; import
- * cucumber.api.java.en.And; import cucumber.api.java.en.Given; import
- * cucumber.api.java.en.Then; import cucumber.api.java.en.When;
- * 
- * public class tcTwoStepDef extends BaseClass { testCaseTwoPOM objTwo = new
- * testCaseTwoPOM(driver);
- * 
- * @Given("^Initialize the browser$") public void initialize_the_browser()
- * throws Throwable { driver = initializeBrowser(); }
- * 
- * @When("^Website is opened search (.+) using search bar$") public void
- * website_is_opened_search_using_search_bar(String key) throws Throwable {
- * objTwo.getSearchBox().sendKeys(key, Keys.ENTER); }
- * 
- * @Then("^Print the result found$") public void print_the_result_found() throws
- * Throwable { String getDetails = objTwo.getSearchResult().getText();
- * System.out.println(getDetails); }
- * 
- * @And("^Open the website$") public void open_the_website() throws Throwable {
- * driver.get(prop.getProperty("url")); }
- * 
- * @And("^close the browser $") public void close_the_browser() throws Throwable
- * { driver.close(); driver = null; } }
- */
+package MS4.stepDefinition;
+
+import MS4.POM.testCaseTwoPOM;
+import MS4.base.BaseClass;
+import cucumber.api.PendingException;
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class tcTwoStepDef extends BaseClass {
+	testCaseTwoPOM objTwo = new testCaseTwoPOM(driver);
+
+	@Given("^Initialize the browser$")
+	public void initialize_the_browser() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Given("^Open the website$")
+	public void open_the_website() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^Website is opened click on shop product in navigation bar$")
+	public void website_is_opened_click_on_shop_product_in_navigation_bar() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^under Shop Product select powder from dropdown$")
+	public void under_Shop_Product_select_powder_from_dropdown() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^Verify if Tide free and gentle is displayed or not$")
+	public void verify_if_Tide_free_and_gentle_is_displayed_or_not() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^close the browser$")
+	public void close_the_browser() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^Website is opened search antibacterial using search bar$")
+	public void website_is_opened_search_antibacterial_using_search_bar() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^Print the result found$")
+	public void print_the_result_found() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@When("^Website is opened Click on learn more under How to do laundry$")
+	public void website_is_opened_Click_on_learn_more_under_How_to_do_laundry() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^Look for How to read laundry symbols from grid$")
+	public void look_for_How_to_read_laundry_symbols_from_grid() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+	@Then("^print the text and validate$")
+	public void print_the_text_and_validate() throws Throwable {
+		// Write code here that turns the phrase above into concrete actions
+		throw new PendingException();
+	}
+
+}
